db926077aa6ce0c08bcca122f452b60c4284997c
Guilherme Salustiano <guissalustiano@gmail.com>
2021-03-28 17:58:20 -0300

    Add relation

--- src/main/kotlin/com/example/demo/category/CategoryEntity.kt
+++ src/main/kotlin/com/example/demo/category/CategoryEntity.kt
@@ -1,11 +1,23 @@
 package com.example.demo.category
 
 import com.example.demo.base.BaseEntity
+import com.example.demo.product.ProductEntity
 import com.example.demo.util.toSlug
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.OneToMany
+import javax.validation.constraints.NotNull
 
 @Entity
 data class CategoryEntity(
+    @NotNull
+    @Column(unique = true, nullable = false)
     var name: String,
-    var slug: String? = name.toSlug(),
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    var slug: String = name.toSlug(),
+
+    @OneToMany
+    var products: MutableList<ProductEntity>? = mutableListOf(),
 ) : BaseEntity()
--- src/main/kotlin/com/example/demo/product/ProductEntity.kt
+++ src/main/kotlin/com/example/demo/product/ProductEntity.kt
@@ -1,11 +1,26 @@
 package com.example.demo.product
 
 import com.example.demo.base.BaseEntity
+import com.example.demo.category.CategoryEntity
 import com.example.demo.util.toSlug
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.ManyToOne
+import javax.validation.constraints.NotNull
 
 @Entity
 data class ProductEntity(
+    @NotNull
+    @Column(unique = true, nullable = false)
     var name: String,
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+
     var slug: String? = name.toSlug(),
+
+    var description: String? = "",
+
+    @ManyToOne
+    var category: CategoryEntity? = null,
 ) : BaseEntity()
